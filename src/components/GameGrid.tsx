4044704fbcad87c028dc729790e1f78ff38e68d3
Izaan khalid <izaankhalid@Izaans-MBP.attlocal.net>
2024-06-17 14:01:24 -0500

    Create a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface Game {
-	id: number;
-	name: string;
-}
-
-interface FetchGamesResponse {
-	count: number;
-	results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-	const [games, setGames] = useState<Game[]>([]);
-	const [err, setErr] = useState("");
-
-	useEffect(() => {
-		apiClient
-			.get<FetchGamesResponse>("/xgames")
-			.then((res) => setGames(res.data.results))
-			.catch((err) => setErr(err.message));
-	});
+	const { games, err } = useGames();
 
 	return (
 		<>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '316f4f6a6ba04d4d82d353a96bdfea54'
+        key: import.meta.env.VITE_API_KEY
     }
 })
