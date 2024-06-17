5c96206a5c04b94dd851982bc1c0f33c6b891573
Izaan Khalid <izaan.khalid@outlook.com>
2024-06-17 17:57:06 -0500

    Display Genres

--- src/components/GameCardComponent.tsx
+++ src/components/GameCardComponent.tsx
@@ -7,7 +7,7 @@ interface GameCardComponentProps {
 
 const GameCardComponent = ({ children }: GameCardComponentProps) => {
 	return (
-		<Box width="300px" borderRadius={10} overflow={"hidden"}>
+		<Box borderRadius={10} overflow={"hidden"}>
 			{children}
 		</Box>
 	);
