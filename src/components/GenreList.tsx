3725fdcd9f92d3c12facbfd8e2e482ba6ba81fb7
Izaan Khalid <izaan.khalid@outlook.com>
2024-06-17 20:36:54 -0500

    Clean up genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
 	Button,
 	HStack,
+	Heading,
 	Image,
 	List,
 	ListItem,
@@ -22,35 +23,43 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
 
 	if (err) return null;
 	return (
-		<List>
-			{isLoading &&
-				skeletons.map((skeleton) => (
-					<GenreListSekeleton key={skeleton} />
+		<>
+			<Heading fontSize="2xl" marginBottom={2}>
+				Genres
+			</Heading>
+			<List>
+				{isLoading &&
+					skeletons.map((skeleton) => (
+						<GenreListSekeleton key={skeleton} />
+					))}
+				{data.map((genre) => (
+					<ListItem key={genre.id} paddingY="5px">
+						<HStack>
+							<Image
+								boxSize="32px"
+								borderRadius={8}
+								src={getCroppedImageUrl(genre.image_background)}
+								objectFit="cover"
+							/>
+							<Button
+								whiteSpace="normal"
+								textAlign="left"
+								onClick={() => onSelectGenre(genre)}
+								fontWeight={
+									genre.id === selectedGenre?.id
+										? "bold"
+										: "normal"
+								}
+								fontSize="lg"
+								variant="link"
+							>
+								{genre.name}
+							</Button>
+						</HStack>
+					</ListItem>
 				))}
-			{data.map((genre) => (
-				<ListItem key={genre.id} paddingY="5px">
-					<HStack>
-						<Image
-							boxSize="32px"
-							borderRadius={8}
-							src={getCroppedImageUrl(genre.image_background)}
-						/>
-						<Button
-							onClick={() => onSelectGenre(genre)}
-							fontWeight={
-								genre.id === selectedGenre?.id
-									? "bold"
-									: "normal"
-							}
-							fontSize="lg"
-							variant="link"
-						>
-							{genre.name}
-						</Button>
-					</HStack>
-				</ListItem>
-			))}
-		</List>
+			</List>
+		</>
 	);
 };
 
