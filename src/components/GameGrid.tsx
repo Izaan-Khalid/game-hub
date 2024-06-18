56bf7c58ca8526ffb693924c2a41e825148d3664
Izaan Khalid <izaan.khalid@outlook.com>
2024-06-17 19:28:25 -0500

    Filter games by platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,5 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardComponent from "./GameCardComponent";
@@ -7,10 +7,11 @@ import { Genre } from "../hooks/useGenres";
 
 interface GameGridProps {
 	selectedGenre: Genre | null;
+	selectedPlatform: Platform | null;
 }
 
-const GameGrid = ({ selectedGenre }: GameGridProps) => {
-	const { data, err, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: GameGridProps) => {
+	const { data, err, isLoading } = useGames(selectedGenre, selectedPlatform);
 	const skeletons = [1, 2, 3, 4, 5, 6];
 
 	return (
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,19 +1,33 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
-const PlatformSelector = () => {
+interface PlatformSelectorProp {
+	onSelectPlatform: (platform: Platform) => void;
+	selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({
+	onSelectPlatform,
+	selectedPlatform,
+}: PlatformSelectorProp) => {
 	const { data, err } = usePlatforms();
 
 	if (err) return null;
 	return (
 		<Menu>
 			<MenuButton as={Button} rightIcon={<BsChevronDown />}>
-				Platforms
+				{selectedPlatform?.name || "Platforms"}
 			</MenuButton>
 			<MenuList>
 				{data.map((platform) => (
-					<MenuItem key={platform.id}>{platform.name}</MenuItem>
+					<MenuItem
+						onClick={() => onSelectPlatform(platform)}
+						key={platform.id}
+					>
+						{platform.name}
+					</MenuItem>
 				))}
 			</MenuList>
 		</Menu>
