33c0b75ebc6354b61eb071e6aff5718ee41ffcda
Izaan Khalid <izaan.khalid@outlook.com>
2024-06-17 22:24:59 -0500

    Added API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: import.meta.env.VITE_API_KEY
-    }
-})
+	baseURL: "https://api.rawg.io/api",
+	params: {
+		key: "316f4f6a6ba04d4d82d353a96bdfea54",
+	},
+});
